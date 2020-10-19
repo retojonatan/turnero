a3f63c40dd0c96ae736c87069cd3ba459bd7289c
Pyrex <retojonatan@gmail.com>
2020-10-18 23:07:56 -0300

    cambios de fecha y control del día

--- js/turnos.js
+++ js/turnos.js
@@ -15,13 +15,57 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const ccjTurnos = db.collection('ccj-turnos');
 
+const tabla = document.querySelector('#tablaTurnos');
+const tablahoy = document.querySelector('#tablaHoy');
+
+ccjTurnos.get().then((consulta) => {
+  consulta.docs.forEach(doc => {
+    renderHoy(doc);
+  })
+})
+
+function renderHoy(doc) {
+  var fechaDB = ccjTurnos.doc(doc.id);
+  if (esHoy(fechaDB.id)) {
+    fechaDB.get().then(function (doc) {
+      fechaDB.collection('Clientes').get().then(function (horariosDB) {
+        horariosDB.forEach(function (horaDB) {
+          if (horaDB.id !== 'Created' && (horaDB.get('Estado') === 'Activo')) {
+            let tbody = document.createElement('tbody');
+            let fila = document.createElement('tr');
+            let td_fecha = document.createElement('td');
+            let td_hora = document.createElement('td');
+            let td_nombre = document.createElement('td');
+            let td_telefono = document.createElement('td');
+            let td_mail = document.createElement('td');
+            let td_motivo = document.createElement('td');
+            td_fecha.textContent = doc.id;
+            td_hora.textContent = horaDB.id;
+            td_nombre.textContent = horaDB.get('Nombre');
+            td_telefono.textContent = horaDB.get('Telefono');
+            td_mail.textContent = horaDB.get('Mail');
+            td_motivo.textContent = horaDB.get('Motivo');
+            fila.appendChild(td_fecha);
+            fila.appendChild(td_hora);
+            fila.appendChild(td_nombre);
+            fila.appendChild(td_telefono);
+            fila.appendChild(td_mail);
+            fila.appendChild(td_motivo);
+            tbody.appendChild(fila)
+            tablaHoy.appendChild(tbody);
+          }
+        })
+      })
+    })
+  }
+}
+
 ccjTurnos.get().then((consulta) => {
   consulta.docs.forEach(doc => {
     renderFechas(doc);
   })
 })
 
-const tabla = document.querySelector('#tablaTurnos');
 
 function renderFechas(doc) {
   var fechaDB = ccjTurnos.doc(doc.id);
@@ -81,14 +125,36 @@ function compararFecha(fecha) {
     mm = '0' + mm;
   }
   fechaArray = fecha.split('-');
-  fechaDD = fechaArray[0];
+  fechaDD = fechaArray[2];
   fechaMM = fechaArray[1];
-  fechaYYYY = fechaArray[2];
+  fechaYYYY = fechaArray[0];
   if (fechaYYYY >= yyyy) {
     if (fechaMM > mm) {
       return true;
     } else if (fechaMM = mm) {
       return fechaDD >= dd;
+    }
+  }
+}
+
+function esHoy(fecha) {
+  var hoy = new Date();
+  var dd = hoy.getDate();
+  var mm = hoy.getMonth() + 1;
+  var yyyy = hoy.getFullYear();
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  fechaArray = fecha.split('-');
+  fechaDD = fechaArray[2];
+  fechaMM = fechaArray[1];
+  fechaYYYY = fechaArray[0];
+  if (fechaYYYY == yyyy) {
+    if (fechaMM == mm) {
+      return fechaDD == dd;
     }
   }
 }
